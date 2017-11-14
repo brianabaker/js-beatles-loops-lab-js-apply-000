51256e66281ca1e689468d411f00627bffbed597
Briana Baker <brianaclairbaker@gmail.com>
2017-11-14 13:02:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var musicians = [];
+var instruments = [];
+
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+  for(var i = 0; i < musicians.length; i++){
+    newArr.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+    return newArr;
+}
+
+function johnLennonFacts(facts){
+  var loud = [];
+  var i = 0;
+  while (i < facts.length){
+    loud.push(facts[i] + "!!!");
+    i++ ;
+  }
+  return loud;
+}
